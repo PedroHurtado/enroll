38554b94751dc3dff872835729fcc2497229531f
Pedro Hurtado <pedrohurt@gmail.com>
2024-12-30 18:30:57 +0100

    implements NG_VALUE_ACCESSOR en ImageUploadComponent

--- src/app/components/imageupload/imageupload.component.ts
+++ src/app/components/imageupload/imageupload.component.ts
@@ -1,32 +1,76 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-imageupload',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './imageupload.component.html',
-  styleUrls: ['./imageupload.component.css']
+  styleUrls: ['./imageupload.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: ImageuploadComponent,
+      multi: true
+    }
+  ]
 })
-export class ImageuploadComponent {
-  imageUrl: string | null = null;
+export class ImageuploadComponent implements ControlValueAccessor {
+  imageFile: File | null = null;
+  preview:any = null;
   isDragging = false;
+  disabled = false;
 
-  onFileSelected(event: any): void {
-    const file = event.target.files[0];
-    if (file) {
+  // ControlValueAccessor methods
+  onChange: any = () => {};
+  onTouch: any = () => {};
+
+  writeValue(value: File): void {
+    this.imageFile = value || null
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onFileSelected(event: Event): void {
+    event.preventDefault();
+    event.stopPropagation
+    if (this.disabled) return;
+    const input = (event.target as HTMLInputElement)
+    if (input.files && input.files.length > 0) {
+      this.processFile(input.files[0]);
+    }
+  }
+
+  private processFile(file: File): void {
+    if (file.type.startsWith('image/')) {
       const reader = new FileReader();
       reader.onload = (e: any) => {
-        this.imageUrl = e.target.result;
+        this.imageFile =file
+        this.preview = e.target.result
+        this.onChange(this.imageFile);
+        this.onTouch();
       };
       reader.readAsDataURL(file);
     }
   }
 
   deleteImage(): void {
-    this.imageUrl = null;
+    if (this.disabled) return;
+    this.imageFile =null
+    this.onChange(this.imageFile);
+    this.onTouch();
   }
 
   onDragOver(event: DragEvent): void {
@@ -42,6 +86,7 @@ export class ImageuploadComponent {
   }
 
   onDrop(event: DragEvent): void {
+    if (this.disabled) return;
     event.preventDefault();
     event.stopPropagation();
     this.isDragging = false;
@@ -49,13 +94,7 @@ export class ImageuploadComponent {
     const files = event.dataTransfer?.files;
     if (files && files.length > 0) {
       const file = files[0];
-      if (file.type.startsWith('image/')) {
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-          this.imageUrl = e.target.result;
-        };
-        reader.readAsDataURL(file);
-      }
+      this.processFile(file);
     }
   }
 }
--- src/app/pages/enroll/steppers/components/stepfather/stepfather.component.ts
+++ src/app/pages/enroll/steppers/components/stepfather/stepfather.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { ImageuploadComponent } from '../../../../../components/imageupload/imageupload.component';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-stepfather',
-  imports: [ImageuploadComponent],
+  imports: [ImageuploadComponent, ReactiveFormsModule],
   templateUrl: './stepfather.component.html',
   styleUrl: './stepfather.component.css'
 })
 export class StepfatherComponent {
+  form= new FormGroup({
+    anverso: new FormControl(null),
+    reverso:new FormControl(null),
+  })
+  constructor(form:FormBuilder) {
 
+
+  }
+  submit(){
+    console.log(this.form.value)
+  }
 }
