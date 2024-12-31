443b2d68b7e2b95cd49a71863600818caa38c9c7
Pedro Hurtado <pedrohurt@gmail.com>
2024-12-31 18:19:25 +0100

    situation

--- src/app/pages/enroll/familysituation/familysituation.component.ts
+++ src/app/pages/enroll/familysituation/familysituation.component.ts
@@ -3,12 +3,12 @@ import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
 
-import {CdkTextareaAutosize, TextFieldModule} from '@angular/cdk/text-field';
+import {TextFieldModule} from '@angular/cdk/text-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatButtonModule} from '@angular/material/button';
 import { Router, RouterLink } from '@angular/router';
-import { LogoComponent } from '../../../components/logo/logo.component';
+import { } from '../../../components/logo/logo.component';
 import { HeaderComponent } from '../../../components/header/header.component';
 import { ContainerComponent } from '../../../components/container/container.component';
 @Component({
@@ -19,7 +19,6 @@ import { ContainerComponent } from '../../../components/container/container.comp
     MatRadioModule,
     MatSelectModule,
     FormsModule,
-    CdkTextareaAutosize,
     TextFieldModule,
     RouterLink,
     MatButtonModule,
@@ -33,6 +32,7 @@ import { ContainerComponent } from '../../../components/container/container.comp
 export class FamilysituationComponent {
   identity:string='student';
   situation:string='married' ;
+  custody:string="mother"
   form=new FormGroup({
 
   })
