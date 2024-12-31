e4bc3c1fccf89dcaac2ae82e893b7b10def2b793
Pedro Hurtado <pedrohurt@gmail.com>
2024-12-31 13:44:42 +0100

    add course 2022-2023

--- src/app/pages/enroll/enroll.service.ts
+++ src/app/pages/enroll/enroll.service.ts
@@ -6,7 +6,7 @@ const registrations: Enroll[] = [
   { id: 2, studentId: 2, studentName: 'Jane Doe', courseId: 2, courseName: 'React', start: 2021, end: 2022, status:'open' },
   { id: 3, studentId: 3, studentName: 'Sam Doe', courseId: 3, courseName: 'Vue', start: 2021, end: 2022, status:'closed' },
   { id: 4, studentId: 4, studentName: 'Sara Doe', courseId: 4, courseName: 'Node', start: 2021, end: 2022,status:'closed'},
-  { id: 5, studentId: 5, studentName: 'Tom Doe', courseId: 5, courseName: 'MongoDB', start: 2021, end: 2022,status:'closed' },
+  { id: 5, studentId: 5, studentName: 'Tom Doe', courseId: 5, courseName: 'MongoDB', start: 2022, end: 2023,status:'closed' },
 ]
 
 @Injectable({
