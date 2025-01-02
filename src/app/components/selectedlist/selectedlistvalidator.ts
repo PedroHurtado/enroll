fcdd691f2886a811e888be2b26d695d674cde85b
Pedro Hurtado <pedrohurt@gmail.com>
2025-01-02 13:19:13 +0100

    add form to steep_alumno

--- src/app/components/selectedlist/selectedlistvalidator.ts
+++ src/app/components/selectedlist/selectedlistvalidator.ts
@@ -3,6 +3,6 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 export function selectedlistValidator(length:number): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null  => {
     const selectedItems = control.value;
-    return selectedItems.length === length ? null : { selectedlist: true}
+    return selectedItems?.length === length ? null : { selectedlist: true}
   };
 }
--- src/app/pages/enroll/steppers/components/stepalumno/stepalumno.component.ts
+++ src/app/pages/enroll/steppers/components/stepalumno/stepalumno.component.ts
@@ -3,17 +3,31 @@ import { ImageuploadComponent } from '../../../../../components/imageupload/imag
 import { FileuploadComponent } from '../../../../../components/fileupload/fileupload.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { selectedlistValidator } from '../../../../../components/selectedlist/selectedlistvalidator';
 @Component({
   selector: 'app-stepalumno',
   imports: [
     ImageuploadComponent,
     FileuploadComponent,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    ReactiveFormsModule
   ],
   templateUrl: './stepalumno.component.html',
   styleUrl: './stepalumno.component.css'
 })
 export class StepalumnoComponent {
+  protected form=new FormGroup({
+    nre: new FormControl(null, Validators.required),
+    dni_observe: new FormControl(null, selectedlistValidator(1)),
+    dni_reverse: new FormControl(null, selectedlistValidator(1)),
+    academic_record:new FormControl(null,),
+    healthcards_observe:new FormControl(null, selectedlistValidator(1)),
+    healthcards_reverse:new FormControl(null, selectedlistValidator(1)),
+  })
+  submit(){
+    console.log(this.form.value);
+  }
 
 }
