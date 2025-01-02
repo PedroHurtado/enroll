40c98c7e3075fd6f5875482a2b8dc32df6fb90a3
Pedro Hurtado <pedrohurt@gmail.com>
2025-01-02 13:58:38 +0100

    forms steppers

--- src/app/pages/enroll/steppers/components/stepothers/stepothers.component.ts
+++ src/app/pages/enroll/steppers/components/stepothers/stepothers.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { FileuploadComponent } from '../../../../../components/fileupload/fileupload.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { selectedlistValidator } from '../../../../../components/selectedlist/selectedlistvalidator';
 
 @Component({
   selector: 'app-stepothers',
-  imports: [FileuploadComponent],
+  imports: [FileuploadComponent, ReactiveFormsModule],
   templateUrl: './stepothers.component.html',
   styleUrl: './stepothers.component.css'
 })
 export class StepothersComponent {
-
+  protected form = new FormGroup({
+    school_insurance: new FormControl(null, selectedlistValidator(1)),
+    ampa: new FormControl(null),
+    school_transportation: new FormControl(null),
+  })
+  submit(){
+    console.log(this.form.value);
+  }
 }
