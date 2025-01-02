b811a0611fc0b68c5819dc5b2944be47fcee1ecf
Pedro Hurtado <pedrohurt@gmail.com>
2025-01-02 13:40:29 +0100

    healt steep

--- src/app/pages/enroll/steppers/components/healstep/healstep.component.ts
+++ src/app/pages/enroll/steppers/components/healstep/healstep.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { FileuploadComponent } from '../../../../../components/fileupload/fileupload.component';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { selectedlistValidator } from '../../../../../components/selectedlist/selectedlistvalidator';
 @Component({
   selector: 'app-healstep',
-  imports: [FileuploadComponent, MatCheckboxModule],
+  imports: [FileuploadComponent, MatCheckboxModule, ReactiveFormsModule],
   templateUrl: './healstep.component.html',
   styleUrl: './healstep.component.css'
 })
 export class HealstepComponent {
-
+  protected form=new FormGroup({
+    condition1: new FormControl(null),
+    condition2: new FormControl(null),
+    condition3: new FormControl(null),
+    condition4: new FormControl(null),
+    files: new FormControl(null)
+  });
+  submit(){
+    console.log(this.form.value);
+  }
 }
