c5db97f22f8356a4b28d17acc1638aaa816396fd
Pedro Hurtado <pedrohurt@gmail.com>
2025-01-04 18:02:14 +0100

    courses servies

--- src/app/backoffice/pages/courses/courses.service.ts
+++ src/app/backoffice/pages/courses/courses.service.ts
@@ -22,10 +22,10 @@ export class CoursesService {
     return newCourse
   }
   update(course: Courses){
-
+    const index = courses.findIndex(c => course)
+    courses[index] = course
   }
   remove(course: Courses) {
-
-
+    courses.splice(courses.indexOf(course), 1)
   }
 }
