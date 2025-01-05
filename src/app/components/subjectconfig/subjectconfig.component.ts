ca05bdc2aa9ff4c2aef9210a287c0a15d6adc8a6
Pedro Hurtado <pedrohurt@gmail.com>
2025-01-05 10:41:47 +0100

    subject config

--- src/app/components/subjectconfig/subjectconfig.component.ts
+++ src/app/components/subjectconfig/subjectconfig.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -23,12 +23,20 @@ import { MatSelectModule } from '@angular/material/select';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SubjectconfigComponent {
+  protected type=signal("all")
   protected form=new FormGroup({
-    text:new FormControl(null),
+    text:new FormControl(null,Validators.required),
     type:new FormControl("all"),
     multiple:new FormControl(true),
     limit:new FormControl(0)
   })
+  constructor(){
+    this.form.get('type')?.valueChanges.subscribe((newValue) => {
+      if(newValue){
+        this.type.set(newValue)
+      }
+    });
+  }
   submit(){
     console.log(this.form.value)
   }
