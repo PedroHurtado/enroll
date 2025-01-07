1e42e9980bea4051b34619a9c6eef6ef91b998ee
Pedro Hurtado <pedrohurt@gmail.com>
2025-01-07 13:22:15 +0100

    levels

--- markdown/levels.ts
+++ markdown/levels.ts
@@ -3,7 +3,7 @@ interface Descriptor {
   name: string
 }
 interface Level extends Descriptor {
-  courses: Courses[]
+  courses: Course[]
 }
 interface Subject {
   title: string,
@@ -13,30 +13,103 @@ interface Subject {
   defaultSubject: any
   subjects: Descriptor[]
 }
-interface Courses extends Descriptor {
-  modalities: Modalities[],
+interface Course extends Descriptor {
+  modalities: Mode[],
   subjects: Subject[]
 }
-interface Modalities extends Descriptor {
+interface Mode extends Descriptor {
   subjects: Subject[]
+}
+class LevelDomain {
+  protected _courses: Course[] = []
+  protected  _id: string
+  protected  _name: string
+  private constructor({ id, name }: { id: string; name: string }) {
+    this._id = id
+    this._name = name
+  }
+  update({ name }: { name: string }){
+    this._name = name
+  }
+  addCourse(course: Course) {
+    this._courses.push(course)
+  }
+  removeCourse(course: Course) {
+    const index = this._courses.findIndex(c => c.id === course.id)
+    if (index !== 1) {
+      this._courses.splice(index, 1)
+    }
+  }
+  updateCourse({ id, name }: { id: string; name: string }) {
+    const index = this._courses.findIndex(c => c.id === id)
+    if (index !== 1) {
+      const updatedCourse = this._courses[index]
+      updatedCourse.name = name
+    }
+  }
+  get courses(): Course[] {
+    return [...this._courses]
+  }
+  get id(): string {
+    return this._id
+  }
+  get name(): string {
+    return this._name
+  }
+  static create(name: string): LevelDomain {
+    const descriptor = createDescriptor(name)
+    return new LevelDomain(descriptor)
+  }
+
 }
 
-const level: Level = {
-  id: '1',
-  name: 'ESO',
-  courses: [
-    {
-      id: '1', name: '1º Eso', modalities: [
-        {
-          id: '1',
-          name: 'Ciencias y tecnología',
-          subjects: []
-        }
-      ],
-      subjects: []
+class CourseDomain{
+  protected _id: string
+  protected _name: string
+  protected _modalities:Mode[] =[]
+  private constructor({ id, name }: { id: string; name: string }){
+    this._id = id
+    this._name = name;
+  }
+  update({ name }: {  name: string }){
+    this._name = name
+  }
+  addMode(mode:Mode){
+    this._modalities.push(mode)
+  }
+  updateMode({ id, name }: { id: string; name: string }){
+    const index = this._modalities.findIndex(m=>m.id===id)
+    if(index!==-1){
+      const updatedMode = this._modalities[index]
+      updatedMode.name = name
     }
-  ]
+  }
+  removeMode(mode:Mode){
+    const index = this._modalities.findIndex(m=>m.id===mode.id)
+    if(index!==-1){
+      this._modalities.splice(index,1)
+    }
+  }
+  get modalities():Mode[]{
+    return [...this._modalities]
+  }
+  get id(): string {
+    return this._id
+  }
+  get name(): string {
+    return this._name
+  }
+  static create(name:string):CourseDomain{
+    const descriptor = createDescriptor(name)
+    return new CourseDomain(descriptor)
+  }
 }
-const descriptor:Descriptor= level;
+function createDescriptor(name: string): Descriptor {
+  return {
+    id: crypto.randomUUID(),
+    name
+  }
+}
+
 
 
