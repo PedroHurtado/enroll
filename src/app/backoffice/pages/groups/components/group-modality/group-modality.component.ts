93f5aa0e064684e5417d74745b3ac41caba461f1
Pedro Hurtado <pedrohurt@gmail.com>
2025-01-21 10:59:07 +0100

    implements drag and drog

--- src/app/backoffice/pages/groups/components/group-modality/group-modality.component.ts
+++ src/app/backoffice/pages/groups/components/group-modality/group-modality.component.ts
@@ -2,15 +2,13 @@ import { Component, input } from '@angular/core';
 import { Modality } from '../../groups.service';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
-import {
-  CdkDrag,
-} from '@angular/cdk/drag-drop';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-group-modality',
   imports: [
     MatButtonModule,
     RouterLink,
-    CdkDrag
+    DragDropModule
   ],
   templateUrl: './group-modality.component.html',
   styleUrl: './group-modality.component.css'
