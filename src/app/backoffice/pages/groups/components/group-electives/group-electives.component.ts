a3cba25779dafc12e9abd63df9b652f954ec8362
Pedro Hurtado <pedrohurt@gmail.com>
2025-01-21 10:36:31 +0100

    drag preview

--- src/app/backoffice/pages/groups/components/group-electives/group-electives.component.ts
+++ src/app/backoffice/pages/groups/components/group-electives/group-electives.component.ts
@@ -2,14 +2,18 @@ import { Component, input } from '@angular/core';
 import { PositionWhitEnrolls } from '../../groups.service';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
-import {CdkDragStart, DragDropModule} from '@angular/cdk/drag-drop';
+import {
+  CdkDragPreview,
+  CdkDrag,
+
+} from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-group-electives',
   imports: [
     MatButtonModule,
     RouterLink,
-    DragDropModule
-
+    CdkDrag,
+    CdkDragPreview
   ],
   templateUrl: './group-electives.component.html',
   styleUrl: './group-electives.component.css'
@@ -21,8 +25,5 @@ export class GroupElectivesComponent {
   protected getEnrollsCountByPosition(elective: any, position: number): number {
     const pos = elective.positions.find((p: any) => p.position === position);
     return pos ? pos.enrolls.length : 0;
-  }
-  onDragStarted(event: CdkDragStart){
-    event.source.element.nativeElement.style.transform = 'none';
   }
 }
